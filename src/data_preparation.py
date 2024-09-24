559945b1473c8b1c9f0d6c128ebfcfbae6384e18
Prabhat Kumar Jha <prabhatbit2016@gmail.com>
2024-09-24 16:14:38 +0530

    changed backward slash to forward slash in data prep file

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    data=pd.read_csv('C:\Users\prabhat.j\Downloads\route optimize\data\delivery_data.csv')
+    data=pd.read_csv('C:/Users/prabhat.j/Downloads/route optimize/data/delivery_data.csv')
     print(data.head())
     return data
 
