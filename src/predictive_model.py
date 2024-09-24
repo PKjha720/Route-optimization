76df98a162c6d5a52cd24c2923b65c5d5aaecd36
Prabhat Kumar Jha <prabhatbit2016@gmail.com>
2024-09-24 16:11:05 +0530

    changed 6th line in predictive_model.py

--- src/predictive_model.py
+++ src/predictive_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-from data_preparation import load_data
+from src.data_preparation import load_data
 
 def build_predictive_model():
     data = load_data()
